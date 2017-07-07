74363ed4dbc2f414e1aba61c8aff3bcc6ad303c4
kaalm1 <kaalm1@gmail.com>
2017-07-07 13:43:05 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,19 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: {in: ["Fiction","Non-Fiction"]}
+  before_validation :clickbait
+
+  def clickbait
+
+   return false if self.title.nil?
+
+   arr = ["Won't Believe","Secret","Top [number]", "Guess"]
+   title_status = arr.select do |phrase|
+     self.title.include?(phrase)
+   end.empty?
+   !title_status
+  end
+
 end
